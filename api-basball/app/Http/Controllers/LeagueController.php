ab3615a5032883b620fffcd0c6803eab0970c72b
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-25 23:34:32 -0300

    criação do model, controller e migration de League

---
+++ api-basball/app/Http/Controllers/LeagueController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\League;
+use Illuminate\Http\Request;
+
+class LeagueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(League $league)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, League $league)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(League $league)
+    {
+        //
+    }
+}
