8f81733a112ccf6e6b9948c7a765d72578fcd5ad
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-26 20:44:01 -0300

    criação dos models, controllers e migrations de player, contract, coach e inventory

---
+++ api-basball/app/Http/Controllers/CoachController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coach;
+use Illuminate\Http\Request;
+
+class CoachController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Coach $coach)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Coach $coach)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Coach $coach)
+    {
+        //
+    }
+}
---
+++ api-basball/app/Http/Controllers/PlayerController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Player;
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Player $player)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Player $player)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Player $player)
+    {
+        //
+    }
+}
