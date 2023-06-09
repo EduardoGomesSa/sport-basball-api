77fe9611d08f594000409f681d35c44a7dac3c67
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-06-09 00:17:47 -0300

    implementação do método de criar instituição

--- api-basball/app/Http/Controllers/InstitutionController.php
+++ api-basball/app/Http/Controllers/InstitutionController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\InstitutionRequest;
+use App\Http\Resources\InstitutionResource;
 use App\Models\Institution;
 use Illuminate\Http\Request;
 
 class InstitutionController extends Controller
 {
+    private $institution;
+
+    public function __construct(Institution $institution)
+    {
+        $this->institution = $institution;
+    }
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(InstitutionRequest $request)
     {
+        $institutionExist = $this->institution->where('name', $request->name)->first();
 
+        if($institutionExist != null){
+            return response(['error'=>'institution already exist in database'])->setStatusCode(400);
+        }
+
+        $newInstitution = $this->institution->create($request->all());
+
+        $resource = new InstitutionResource($newInstitution);
+
+        return $resource->response(['message'=>'Institution successfull created']);
     }
 
     /**
