27dbc40b37a54b5fb42b7e2038c2c6f0173564e5
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-20 23:35:30 -0300

    criação dos escopos dos métodos do model round

--- app/Http/Controllers/RoundController.php
+++ app/Http/Controllers/RoundController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class RoundController extends Controller
 {
-    //
+    public function index(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function update(){
+
+    }
 }
